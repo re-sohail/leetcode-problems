0ec2782711cd1682952765a162dbf5bebc784320
sohail-fabtech <sohail.khan@fabtechsol.com>
2025-05-11 23:18:54 +0500

    update

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
-// 1. Broute Method -> Time Complexity -> O(n2)
-function BruteMethod(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-}
+// function app(nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
 
-// console.log(BruteMethod([2, 3, 20, 9, 5, 13, 34], 9));
+// function app(nums, target) {
+//   const map = new Map();
 
-// 2. Mapping -> useing two loops but Time Complexity -> O(n)
-function MappingMethod(nums, target) {
-  const map = new Map(); // there are methods are has, set, get and etc
+//   nums.forEach((item, i) => map.set(item, i));
 
-  nums.forEach((value, i) => {
-    map.set(value, i);
-  });
+//   for (let i = 0; i < nums.length; i++) {
+//     const remaining = target - nums[i];
+//     if (map.has(remaining) && map.get(remaining) !== i) {
+//       return [i, map.get(remaining)];
+//     }
+//   }
+// }
 
-  for (let i = 0; i < nums.length; i++) {
-    const remaining = target - nums[i];
-    if (map.has(remaining) && map.get(remaining) !== i) {
-      return [i, map.get(remaining)];
-    }
-  }
-}
+// function app(nums, target) {
+//   const map = new Map();
 
-// console.log(MappingMethod([2, 3, 20, 7, 5, 13, 34], 5));
+//   for (let i = 0; i < nums.length; i++) {
+//     const remaining = target - nums[i];
 
-// 3. Mapping with single Loop -> Time Complexity -> O(n)
-function MapWithSingleLoop(nums, target) {
-  const map = new Map();
+//     if (map.has(remaining)) {
+//       return [map.get(remaining), i];
+//     }
+//     map.set(nums[i], i);
+//   }
+// }
 
-  for (let i = 0; i < nums.length; i++) {
-    const remaining = target - nums[i];
-    if (map.has(remaining)) {
-      return [map.get(remaining), i];
-    }
-    map.set(nums[i], i);
-  }
-}
-
-console.log(MapWithSingleLoop([2, 3, 20, 7, 5, 13, 34], 10));
-
-// 4. With Sorting + Pointer -> Time Complexity -> O(Log(n))
-function SortWithPointer(nums, target) {
-  // pair
-  const arr = nums.map((value, i) => [value, i]);
-  // sort
-  let sortedArr = arr.sort((a, b) => a[0] - b[0]);
+function app(nums, target) {
+  let newArr = nums.map((item, i) => [item, i]);
+  const sortedData = newArr.sort((a, b) => a[0] - b[0]);
 
   let left = 0;
-  let right = sortedArr.length - 1;
+  let right = sortedData.length - 1;
+
   while (left < right) {
-    let sum = arr[left][0] + arr[right][0];
+    let sum = newArr[left][0] + newArr[right][0];
 
     if (sum === target) {
-      return [arr[left][1], arr[right][1]];
+      return [newArr[left][1], newArr[right][1]];
     } else if (sum < target) {
       left++;
     } else {
@@ -66,4 +54,4 @@ function SortWithPointer(nums, target) {
   }
 }
 
-// console.log(SortWithPointer([2, 3, 20, 7, 5, 13, 34], 10));
+console.log(app([2, 3, 5, 8, 7, 9, 4, 2], 15));
